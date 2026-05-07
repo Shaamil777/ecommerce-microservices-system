a0c819ce4058d71f3b1bdc80b72467aab124686f
Shaamil777 <shamilm2324@gmail.com>
2026-05-07 17:12:45 +0530

    feat:added zod validator for authentication methods (register and login)

--- auth-service/src/routes/authRoutes.js
+++ auth-service/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router()
 const {register, login} = require("../controllers/authController")
 const verifyToken = require("../middleware/authMiddleware")
+const validate = require("../middleware/validate")
+const {registerSchema,loginSchema} = require("../validators/authValidator")
 
-router.post("/register",register)
-router.post("/login",login)
+router.post("/register",validate(registerSchema),register)
+router.post("/login",validate(loginSchema),login)
 router.get("/verify",verifyToken,(req,res)=>{
     res.json({userId:req.user.id})
 })
